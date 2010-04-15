4530397a8574719005086f10938ccf4f818caed8
edu.montandon <edu.montandon@6753c531-a96e-cf96-403f-e17fae127fcb>
2010-04-15 01:54:49 +0000

    * Refatoracoes (exclusao da greyImage, criacao da MyImage) * Implementacao do histograma para RGB * Exibicao do histograma na janela principal

--- trunk/ImgHunter/src/imghunter/Histograma.java
+++ trunk/ImgHunter/src/imghunter/Histograma.java
@@ -18,11 +18,11 @@ import org.jfree.data.category.DefaultCategoryDataset;
 public class Histograma {
 
     private JFreeChart grafico;
-    private double[] frequencia = new double[256];
+    private double[][] frequencia = new double[256][3];
     private BufferedImage foto;
 
-    public Histograma(BufferedImage cinza) {
-        this.foto = cinza;
+    public Histograma(BufferedImage img) {
+        this.foto = img;
         this.grafico = this.criaChart();
     }//fim do construtor
 
@@ -35,10 +35,13 @@ public class Histograma {
      */
     private void calculaFrequencia() {
 
+        //armazena o valor de cada variavel no histograma
         for (int x = 0; x < this.foto.getWidth(); x++) {
             for (int y = 0; y < this.foto.getHeight(); y++) {
                 Color cor = new Color(this.foto.getRGB(x, y));
-                this.frequencia[cor.getRed()] += 1;
+                this.frequencia[cor.getRed()][0] += 1;
+                this.frequencia[cor.getGreen()][1] += 1;
+                this.frequencia[cor.getBlue()][2] += 1;
             }//fim do for
         }
     }//fim do calculaFrequencia
@@ -47,12 +50,17 @@ public class Histograma {
         this.calculaFrequencia();
 
         DefaultCategoryDataset dataset = new DefaultCategoryDataset();
-        
+
+        //insere os valores no dataset para carregar no grafico
         for (int x = 0; x < this.frequencia.length; x++) {
-            dataset.addValue(frequencia[x], "", "" + x);
+            String v2 = String.valueOf(x);
+           // String v2 = x == 0 || 16 % x == 0? String.valueOf(x) : "";
+            dataset.addValue(frequencia[x][0], "R", v2);
+            dataset.addValue(frequencia[x][1], "G", v2);
+            dataset.addValue(frequencia[x][2], "B", v2);
         }
 
-        JFreeChart chart = ChartFactory.createBarChart(
+        JFreeChart chart = ChartFactory.createLineChart(
                 "Histograma", // chart title
                 "Pixels", // domain axis label
                 "Quantidade", // range axis label
@@ -64,5 +72,13 @@ public class Histograma {
                 );
         return chart;
     }//fim do criaChart
+
+    /**
+     * Equaliza o histograma
+     */
+    public void equalizarHistograma() {
+        //TODO
+    }
+
 }//fim da classe
 
