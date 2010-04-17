9570aef4fdda9d20dbda242b50377d3576dc7c10
edu.montandon <edu.montandon@6753c531-a96e-cf96-403f-e17fae127fcb>
2010-04-17 18:22:56 +0000

    * Refatoracoes * Implementacao da equalizacao da imagem 	*funcionalidade 90% pronta 	*GUI com a opcao de equalizar pronta

--- ImgHunter/src/imghunter/MyImage.java
+++ ImgHunter/src/imghunter/MyImage.java
@@ -5,16 +5,8 @@
 package imghunter;
 
 import java.awt.Color;
-import java.awt.Graphics;
-import java.awt.GraphicsConfiguration;
-import java.awt.GraphicsDevice;
-import java.awt.GraphicsEnvironment;
-import java.awt.HeadlessException;
 import java.awt.Image;
-import java.awt.Transparency;
 import java.awt.image.BufferedImage;
-import java.awt.image.ColorModel;
-import java.awt.image.PixelGrabber;
 import java.io.File;
 import java.io.IOException;
 import javax.imageio.ImageIO;
@@ -26,9 +18,12 @@ import javax.imageio.ImageIO;
 public class MyImage {
 
     private BufferedImage image;
+    private int[][] frequencia;
 
     public MyImage(String imgOriginal) throws IOException {
         image = ImageIO.read(new File(imgOriginal));
+        this.frequencia = new int[256][3];
+        this.calculaFrequencia();
     }
 
     /**
@@ -56,77 +51,10 @@ public class MyImage {
         return image;
     }
 
-    // This method returns a buffered image with the contents of an image
-    public static BufferedImage toBufferedImage(Image image) {
-        if (image instanceof BufferedImage) {
-            return (BufferedImage) image;
-        }
-
-        // This code ensures that all the pixels in the image are loaded
-        //image = new ImageIcon(image).getImage();
-
-        // Determine if the image has transparent pixels; for this method's
-        // implementation, see Determining If an Image Has Transparent Pixels
-        //  boolean hasAlpha = hasAlpha(image);
-
-        // Create a buffered image with a format that's compatible with the screen
-        BufferedImage bimage = null;
-        GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
-        try {
-            // Determine the type of transparency of the new buffered image
-            int transparency = Transparency.OPAQUE;
-            //          if (hasAlpha) {
-            transparency = Transparency.BITMASK;
-            //       }
-
-            // Create the buffered image
-            GraphicsDevice gs = ge.getDefaultScreenDevice();
-            GraphicsConfiguration gc = gs.getDefaultConfiguration();
-            bimage = gc.createCompatibleImage(
-                    image.getWidth(null), image.getHeight(null), transparency);
-        } catch (HeadlessException e) {
-            // The system does not have a screen
-        }
-
-        if (bimage == null) {
-            // Create a buffered image using the default color model
-            int type = BufferedImage.TYPE_INT_RGB;
-            //          if (hasAlpha) {
-            //             type = BufferedImage.TYPE_INT_ARGB;
-            //         }
-            bimage = new BufferedImage(image.getWidth(null), image.getHeight(null), type);
-        }
-
-        // Copy image to buffered image
-        Graphics g = bimage.createGraphics();
-
-        // Paint the image onto the buffered image
-        g.drawImage(image, 0, 0, null);
-        g.dispose();
-
-        return bimage;
+    public int[][] getFrequencia() {
+        return this.frequencia;
     }
 
-// This method returns true if the specified image has transparent pixels
-    public static boolean hasAlpha(Image image) {
-        // If buffered image, the color model is readily available
-        if (image instanceof BufferedImage) {
-            BufferedImage bimage = (BufferedImage) image;
-            return bimage.getColorModel().hasAlpha();
-        }
-
-        // Use a pixel grabber to retrieve the image's color model;
-        // grabbing a single pixel is usually sufficient
-        PixelGrabber pg = new PixelGrabber(image, 0, 0, 1, 1, false);
-        try {
-            pg.grabPixels();
-        } catch (InterruptedException e) {
-        }
-
-        // Get the image's color model
-        ColorModel cm = pg.getColorModel();
-        return cm.hasAlpha();
-    }
 
     /**
      * redimensiona a imagem
@@ -148,5 +76,83 @@ public class MyImage {
                     BufferedImage.SCALE_SMOOTH);
         }
     }
+
+        /**
+     * Equaliza o histograma
+     */
+    public void equalizar() {
+        int freqA[][] = new int[256][3];
+        int freqEq[][] = new int[256][3];
+
+        // aplica a soma acumulada
+        for(int i = 0; i < frequencia.length; i++) {
+
+            freqA[i][0] += i == 0 ? this.frequencia[i][0] :
+                this.frequencia[i][0] + freqA[i-1][0];
+            freqA[i][1] += i == 0 ? this.frequencia[i][1] :
+                this.frequencia[i][1] + freqA[i-1][1];
+            freqA[i][2] += i == 0 ? this.frequencia[i][2] :
+                this.frequencia[i][2] + freqA[i-1][2];
+        }
+
+        //define numero de pixels para cada cor
+        int npR = freqA[freqA.length][0], npG = freqA[freqA.length][1],
+                npB = freqA[freqA.length][2];
+
+        //divide a frequencia pelo total d pixels e multiplica pelo numero de tons
+        for(int i = 0; i < frequencia.length; i++) {
+            freqA[i][0] = (int)(((double)freqA[i][0] / (double)(npR)) * (double)this.frequencia.length);
+            freqA[i][1] = (int)(((double)freqA[i][1] / (double)(npG)) * (double)this.frequencia.length);
+            freqA[i][2] = (int)(((double)freqA[i][2] / (double)(npB)) * (double)this.frequencia.length);
+        }
+
+        //gera nova frequencia
+        for(int i = 0; i < freqEq.length; i++) {
+
+            int newValR = 0;
+            int newValG = 0;
+            int newValB = 0;
+
+            for(int k = 0; k < freqA.length; k++) {
+                if(freqA[k][0] == i)
+                    newValR += this.frequencia[k][0];
+                if(freqA[k][1] == i)
+                    newValG += this.frequencia[k][1];
+                if(freqA[k][2] == i)
+                    newValB += this.frequencia[k][2];
+            }
+
+            freqEq[i][0] = newValR;
+            freqEq[i][1] = newValG;
+            freqEq[i][2] = newValB;
+
+        }
+
+        this.frequencia = freqEq;
+
+        //falta implementar, aplicar novos tons na imagem
+        for(int i = 0; i < this.image.getWidth(); i++)
+            for(int k = 0; k < this.image.getHeight(); k++)
+            {
+                Color cor = new Color(this.image.getRGB(i, k));
+            }
+
+    }
+
+        /*
+     * Calcula frequencia dos 256 tons de cinza
+     */
+    private void calculaFrequencia() {
+
+        //armazena o valor de cada variavel no histograma
+        for (int x = 0; x < this.image.getWidth(); x++) {
+            for (int y = 0; y < this.image.getHeight(); y++) {
+                Color cor = new Color(this.image.getRGB(x, y));
+                this.frequencia[cor.getRed()][0] += 1;
+                this.frequencia[cor.getGreen()][1] += 1;
+                this.frequencia[cor.getBlue()][2] += 1;
+            }//fim do for
+        }
+    }//fim do calculaFrequencia
 
 }
