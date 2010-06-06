8df8932eb1650c9ce19664aae4e2b236d25bb875
thiakau <thiakau@6753c531-a96e-cf96-403f-e17fae127fcb>
2010-06-06 20:01:19 +0000

    Filtro max e binarização

--- trunk/ImgHunter/src/imghunter/Filtros.java
+++ trunk/ImgHunter/src/imghunter/Filtros.java
@@ -36,26 +36,24 @@ public class Filtros {
           for(alt = 0; alt < img.getHeight(); alt++)
           { 
              // encher a mascara
-              for(int i = larg - maskSize/2 ; i< larg + maskSize/2; i++)
-                  for(int j = alt - maskSize/2; j< alt + maskSize/2 ; j++)
-                          if(i >= 0 && i < img.getWidth())
-                              if(j >= 0 && j < img.getHeight())
-                                  if(i != larg && j != alt)
-                                   mascara.add(img.getRGB(larg,alt));
-
-            // ordena a mascara  
+              for(int i = larg - maskSize/2 ; i<= larg + maskSize/2; i++){
+                  for(int j = alt - maskSize/2; j<= alt + maskSize/2 ; j++){
+                          if(i >= 0 && i < img.getWidth()){
+                              if(j >= 0 && j < img.getHeight()){
+                                 // if(i != larg && j != alt){
+                                    mascara.add( new Color (img.getRGB(i,j)).getRed());
+                                  //   }
+                                } 
+                            }          
+                        } 
+                   }
+            // ordena a mascara
             Collections.sort(mascara);
-           // pega a cor
-           // uma lista pra cada R_G_B
-           ArrayList rList = null, gList = null, bList = null;
-           for(int cores = 0; cores < mascara.size(); cores ++){
-               Color atual = new Color((Integer) mascara.get(cores));
-               rList.add(atual.getRed());
-               gList.add(atual.getGreen());
-               bList.add(atual.getBlue());
-           }
+           
             // grava no arquivo novo
-           imgOut.setRGB(larg, alt,(Integer) mascara.get(mascara.size()-1));
+               Color c =  new Color((Integer)mascara.get(mascara.size()-1),
+                       (Integer)mascara.get(mascara.size()-1),(Integer)mascara.get(mascara.size()-1));
+           imgOut.setRGB(larg, alt,c.getRGB());
 
            // limpa pro próximo pixel
            mascara.clear();
@@ -68,24 +66,31 @@ public class Filtros {
 
 public void binarizacao(BufferedImage img, int limiar, String name) throws IOException
 {
-int larg, alt;
-BufferedImage imgOut = new BufferedImage(
-               img.getWidth(),img.getHeight(), BufferedImage.TYPE_INT_RGB);
-for(larg = 0; larg< img.getWidth(); larg++)
-{       for(alt = 0; alt< img.getHeight(); alt++)
-            {
-              int  aux = img.getRGB(larg, alt);
-              if(aux< limiar)
-                imgOut.setRGB(larg, alt, Color.BLACK.getRGB());
-              else imgOut.setRGB(larg, alt, Color.WHITE.getRGB());
+
+        int lut[] = new int[256];
+
+        for(int i = 0; i < lut.length; i++)
+           lut[i] = i < limiar ? 0: 255;
+
+        BufferedImage imgOut = new BufferedImage(
+                img.getWidth(), img.getHeight(), BufferedImage.TYPE_INT_RGB);
+
+        for (int x = 0; x < imgOut.getWidth(); x++) {
+            for (int y = 0; y < imgOut.getHeight(); y++) {
+
+                Color c = new Color(img.getRGB(x, y));
+                int newColor = lut[c.getRed()];
+                c = new Color(newColor, newColor, newColor);
+                imgOut.setRGB(x, y, c.getRGB());
 
             }
         }
 
-ImageIO.write(imgOut, "JPG", new File("BIN_"+name));
+        ImageIO.write(imgOut, "JPG", new File("BIN_"+name));
 
+    }
 
 }
 
-}
+
 
